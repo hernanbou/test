4cd70be4956c985d17419d7c4c56212d4ff64be3
hernanbou <hernan_bou@hotmail.com>
2023-05-09 01:21:36 -0300

    styled scrollbar

--- src/styles/global.js
+++ src/styles/global.js
@@ -14,32 +14,45 @@ export const GlobalStyle = createGlobalStyle`
         --shape: #ffffff;
     }
 
-    *{
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
-    html {
-        scroll-behavior: smooth;
-    }
-    body, input, textarea, button, select {
-        color: var(--text);
-        font-family: 'Poppins', sans-serif;
-        font-size: 16px;
-        font-weight: 500;
-        background-color: var(--background-page);
-        overflow-x: hidden;
-    }
-    h1, h2, h3, h4, h5, h6, strong {
-        font-weight: 600;
-    }
-    button, select {
-        cursor: pointer;
-    }
-    li{
-        list-style: none;
-    }
-    a{
-        text-decoration: none;
-    }
+
+::-webkit-scrollbar{
+    width: 15px;
+    
+}
+::-webkit-scrollbar-track{
+    background: var(--background);
+}
+::-webkit-scrollbar-thumb{
+    border-radius: 15px;
+    background: var(--pink);
+}
+
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+html {
+    scroll-behavior: smooth;
+}
+body, input, textarea, button, select {
+    color: var(--text);
+    font-family: 'Poppins', sans-serif;
+    font-size: 16px;
+    font-weight: 500;
+    background-color: var(--background-page);
+    overflow-x: hidden;
+}
+h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+}
+button, select {
+    cursor: pointer;
+}
+li{
+    list-style: none;
+}
+a{
+    text-decoration: none;
+}
 `
